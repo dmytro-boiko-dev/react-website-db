5aa6e7349122ae0eda98159302fac323930b627d
dmytro-boiko-dev <dmytroboiko1@gmail.com>
2022-05-29 17:09:53 +0300

    fix: updates for video and routing

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,14 +7,30 @@ export function HeroSection() {
     return (
         <div className='hero-container'>
 
-            <video src="/videos/v13.mp4" autoPlay loop muted />
+            <video src="/videos/v13.mp4"
+                   autoPlay={true}
+                   muted={true}
+                   controls={false}
+                   loop={true}
+                   playsInline={true}
+            />
+
+            {/*<video*/}
+            {/*    autoPlay={true}*/}
+            {/*    muted={true}*/}
+            {/*    controls={false}*/}
+            {/*    loop={true}*/}
+            {/*    playsInline={true}*/}
+            {/*>*/}
+            {/*    <source src="/videos/v13.mp4" type="video/mp4"/>*/}
+            {/*</video>*/}
 
             <h1>DIANA BOIKO</h1>
             <p>CELTA certified English Teacher</p>
             <div className='hero-btns'>
                 <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>ABOUT ME</Button>
                 <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>MY COURSES
-                <i className='far fa-play-circle' />
+                    <i className='far fa-play-circle'/>
                 </Button>
             </div>
         </div>
